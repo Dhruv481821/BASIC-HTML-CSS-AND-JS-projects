b542faa7ad67e54003f626c55b3a2497907fbcf9
Dhruv481821 <sharmadhruv2403@gmail.com>
2026-06-07 12:52:51 +0530

    to-do-list

--- to-doApp/script.js
+++ to-doApp/script.js
@@ -2,16 +2,24 @@ const input = document.querySelector("input");
 const addBtn = document.getElementById("add");
 const taskList = document.getElementById("taskList");
 
-addBtn.addEventListener("click", () => {
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
+addBtn.addEventListener("click", () => {
+    
     if (input.value === "") {
         return;
     }
+    
+    tasks.push(input.value);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 
     const taskRow = document.createElement("div");
 
     const task = document.createElement("p");
     task.innerText = input.value;
+    task.addEventListener("click", () => {
+        task.classList.toggle("completed");
+    });
 
     const deleteBtn = document.createElement("button");
     deleteBtn.innerText = "Delete";
@@ -30,3 +38,23 @@ addBtn.addEventListener("click", () => {
     input.value = "";
 
 });
+
+
+function createTask(taskText) {
+    const taskRow = document.createElement("div");
+
+    const task = document.createElement("p");
+    task.innerText = taskText;
+
+    tasks.push(input.value);
+
+    localStorage.setItem(
+        "tasks",
+        JSON.stringify(tasks)
+    );
+
+createTask(input.value);
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+
+}
